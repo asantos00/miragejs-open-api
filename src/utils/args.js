b9e51c424e8deac2905bf8dfff864a7ba5d9ee9a
João Marques <marques.joaopereira@gmail.com>
2019-12-25 10:58:43 +0000

    refactor: replace minimist with yargs for help page support

--- src/utils/args.js
+++ src/utils/args.js
@@ -1,21 +1,29 @@
 const log = console.log;
 
-const argv = require('minimist')(process.argv.slice(2),
-    {
-        alias: {
-            o: 'output',
-            i: 'input',
-        },
-        default: {
-            o: './mirage.js'
-        }
-    }
-);
+const argv = require('yargs')
+    .option('o', {
+        alias: 'output',
+        describe: 'Output file path with miragejs generated code.',
+        type: 'string',
+        nargs: 1,
+        demand: true,
+        demand: 'An output path is required',
+        default: './miragejs.js'
+    })
+    .option('i', {
+        alias: 'input',
+        describe: 'Input file path with open api specification.',
+        type: 'string',
+        nargs: 1,
+        demand: true,
+        demand: 'An input path is required',
+    }).argv
 
 function getProcessArguments() {
 
     if (!argv.input) {
         log("An input file is required")
+        process.exit();
     }
 
     return argv;
