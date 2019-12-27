ca312f4f85fe9c6514b0ac5ed2b4d56b3c614c4c
Alexandre Santos <alexandre.santozz@gmail.com>
2019-12-27 00:39:17 +0000

    infra: test git hooks 2

--- src/utils/processArguments.js
+++ src/utils/processArguments.js
@@ -1,23 +1,22 @@
 function getProcessArguments() {
-    const argv = require('yargs')
-        .option('o', {
-            alias: 'output',
-            describe: 'Output file path with miragejs generated code.',
-            type: 'string',
-            nargs: 1,
-            default: './miragejs.js'
-        })
-        .option('i', {
-            alias: 'input',
-            describe: 'Input file path/url with open api specification.',
-            type: 'string',
-            nargs: 1,
-            demand: 'An input path/url is required',
-        }).argv
-    return argv;
+  const argv = require("yargs")
+    .option("o", {
+      alias: "output",
+      describe: "Output file path with miragejs generated code.",
+      type: "string",
+      nargs: 1,
+      default: "./miragejs.js"
+    })
+    .option("i", {
+      alias: "input",
+      describe: "Input file path/url with open api specification.",
+      type: "string",
+      nargs: 1,
+      demand: "An input path/url is required"
+    }).argv;
+  return argv;
 }
-
 
 module.exports = {
-    getProcessArguments
-}
+  getProcessArguments
+};
