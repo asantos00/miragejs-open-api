ce124261aeb20651f5d30d4c12eeafa760108ae5
Alexandre Santos <alexandre.santozz@gmail.com>
2019-12-27 00:15:58 +0000

    feat: got it working with open-api v2

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const SwaggerParser = require("swagger-parser");
-const doT = require('dot');
-const fs = require('fs');
+const doT = require("dot");
+const fs = require("fs");
 const prettier = require("prettier");
-const jsf = require('json-schema-faker');
+const jsf = require("json-schema-faker");
 
-const contentType = 'application/json';
+const contentType = "application/json";
 
 doT.templateSettings.strip = false;
 const dots = doT.process({
   path: "./templates"
 });
 
-const replaceParamNotation = (url) => url.replace('{', ':').replace('}', '')
+const replaceParamNotation = url => url.replace(/{/g, ":").replace(/}/g, "");
 
 const generateRouteFromPath = (pathString, pathDefinition) => {
   const verbsInPath = Object.keys(pathDefinition);
@@ -19,20 +19,28 @@ const generateRouteFromPath = (pathString, pathDefinition) => {
     generateHandlerFromVerb(verb, pathString, pathDefinition[verb])
   );
 
-  return handlers.join('\n');
+  return handlers.join("\n");
 };
 
 const generateHandlerFromVerb = (verb, pathString, verbDefinition) => {
+  console.log(`${verb} ${pathString}`);
   const path = replaceParamNotation(pathString);
   const successResponse = verbDefinition.responses[200];
 
   let body = {};
   let statusCode = 200;
 
-  if (successResponse) {
-    const schema = successResponse.content[contentType].schema;
+  if (successResponse && successResponse.schema) {
+    // const schema = successResponse.content[contentType].schema;
+    const schema = successResponse.schema;
 
+    // console.log("Schema:", JSON.stringify(schema, null, 4));
+    jsf.option({
+      alwaysFakeOptionals: true,
+      ignoreProperties: [(property, key) => console.log(property, key)]
+    })
     body = jsf.generate(schema);
+    // console.log("Generated:", JSON.stringify(body, null, 4));
   }
 
   const result = dots.handler({
@@ -48,15 +56,15 @@ const generateHandlerFromVerb = (verb, pathString, verbDefinition) => {
 };
 
 async function run() {
-  const { paths } = await SwaggerParser.dereference("example.yaml");
+  const { paths } = await SwaggerParser.dereference("example-v2.json");
 
   const apiPaths = Object.keys(paths)
     .map(path => generateRouteFromPath(path, paths[path]))
-    .join('\n');
+    .join("\n");
 
-  const prettified = prettier.format(apiPaths, { parser: 'babel' });
+  const prettified = prettier.format(apiPaths, { parser: "babel" });
 
-  fs.writeFileSync('./dist/mirage.js', prettified, 'utf-8');
+  fs.writeFileSync("./dist/mirage.js", prettified, "utf-8");
 }
 
 (async () => {
